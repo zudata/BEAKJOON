0141ad514cdefe10389805fa26b7c721410f2e59
zudata <kxxjx2@gmail.com>
2022-09-27 16:36:02 +0900

    step8 4/6

--- Bystep/step8.py
+++ Bystep/step8.py
@@ -20,3 +20,28 @@ for kk in k:
     l.remove(kk)
 if len(l) == 0: print(-1)
 else: print(sum(l)); print(min(l))
+
+# 11653
+n = int(input())
+ii = []
+i = 2
+while i <= n:
+    if n%i == 0:
+        n = n/i
+        ii.append(i)
+    else: i += 1
+for iii in ii:
+    print(iii)
+
+# 1929
+def isPrime(num):
+    if num==1:
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num%i == 0:
+                return False
+        return True
+m, n = map(int, input().split())
+for j in range(m, n+1):
+    if isPrime(j): print(j)
