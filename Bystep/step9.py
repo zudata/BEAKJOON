c7639506b49ae3772a275cae84e834bce85e17bb
zudata <kxxjx2@gmail.com>
2022-10-02 18:58:35 +0900

    step9 4/11

--- Bystep/step9.py
+++ Bystep/step9.py
@@ -17,3 +17,21 @@ for i in range(n):
 l.sort()
 for j in range(n):
     print(l[j])
+
+# 10989
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+l = [0 for i in range(10001)]
+for i in range(n):
+    l[int(inputs())] += 1
+for j in range(1, 10001):
+    if l[j] != 0:
+        for k in range(l[j]):
+            print(j)
+
+# 25305
+n, k = map(int, input().split())
+l = list(map(int, input().split()))
+l.sort()
+print(l[n-k])
