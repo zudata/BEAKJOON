4a12d58173acc1d8166b83d3c2f1076faa907ac8
zudata <kxxjx2@gmail.com>
2022-10-08 15:16:41 +0900

    step10 2/6

--- Bystep/step10.py
+++ Bystep/step10.py
@@ -6,3 +6,13 @@ def fact(n):
     return result
 n = int(input())
 print(fact(n))
+
+# 10870
+def fibo(n):
+    result = 1
+    if n == 1: result = 1
+    elif n > 1: result = fibo(n-1) + fibo(n-2)
+    else: result = 0
+    return result
+n = int(input())
+print(fibo(n))
