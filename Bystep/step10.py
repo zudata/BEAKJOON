d5117d270dda51d260b0d121c2588184272cdf99
zudata <kxxjx2@gmail.com>
2022-10-11 11:50:05 +0900

    step10 5/6

--- Bystep/step10.py
+++ Bystep/step10.py
@@ -62,7 +62,20 @@ def merge(l, p, q, r):
         i += 1; t += 1
 
 n, k = map(int, input().split())
-5 7
 l = list(map(int, input().split()))
-4 5 1 3 2
 merge_sort(l, 0, n-1)
+
+# 2447
+def star(n):
+    if n == 3: return ['***', '* *', '***']
+    arr = star(n//3)
+    stars = []
+    for i in arr:
+        stars.append(i*3)
+    for i in arr:
+        stars.append(i+' '*(n//3)+i)
+    for i in arr:
+        stars.append(i*3)
+    return stars
+n = int(input())
+print('\n'.join(star(n)))
