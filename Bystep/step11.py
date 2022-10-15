1006f09c4e67e5114caa0a02f3773136c739073b
zudata <kxxjx2@gmail.com>
2022-10-15 11:44:29 +0900

    step11 2/5

--- Bystep/step11.py
+++ Bystep/step11.py
@@ -8,3 +8,14 @@ for i in range(n):
             cards = card[i] + card[j] + card[k]
             if cards <= m and cards >= res: res = cards
 print(res)
+
+# 2231
+n = int(input())
+res = 0
+for i in range(1, n+1):
+    l = list(map(int, str(i)))
+    s = i + sum(l)
+    if s == n:
+        res = i
+        break
+print(res)
