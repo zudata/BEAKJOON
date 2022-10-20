29b8d13bf70009120f003629a8f90432a64ab493
zudata <kxxjx2@gmail.com>
2022-10-20 11:10:04 +0900

    step12 2/7

--- Bystep/step12.py
+++ Bystep/step12.py
@@ -8,3 +8,14 @@ ll = list(map(int, inputs().split()))
 for i in ll:
     if i in l: print(1, end = ' ')
     else: print(0, end = ' ')
+
+# 14425
+n, m = map(int, input().split())
+ll = []; l = set(); count = 0
+for i in range(n):
+    l.add(input())
+for j in range(m):
+    ll.append(input())
+for k in ll:
+    if k in l: count += 1
+print(count)
