6a506a062d7fa4c15a4fff24a5341e23d16f25fd
zudata <kxxjx2@gmail.com>
2022-10-26 17:20:11 +0900

    5597

--- Bystep/step4.py
+++ Bystep/step4.py
@@ -55,3 +55,10 @@ inputs = sys.stdin.readline
 n = int(inputs())
 l = list(map(int, inputs().split()))
 print(l.count(int(inputs())))
+
+# 5597
+l = {i for i in range(1, 31)}
+for j in range(28):
+    l.remove(int(input()))
+for k in list(l):
+    print(k)
