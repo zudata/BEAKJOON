02149faf2e4e698a17263f65a2d23a440029655d
zudata <kxxjx2@gmail.com>
2022-10-27 15:43:03 +0900

    3009

--- Bystep/step14.py
+++ Bystep/step14.py
@@ -5,3 +5,14 @@ x, y, w, h = map(int, inputs().split())
 l = [x, y, w-x, h-y]
 print(min(l))
 
+# 3009
+import sys
+inputs = sys.stdin.readline
+x = []; y = []
+for i in range(3):
+    a, b = list(map(int, inputs().split()))
+    if a not in x: x.append(a)
+    else: x.remove(a)
+    if b not in y: y.append(b)
+    else: y.remove(b)
+print(' '.join(str(j) for j in x+y))
