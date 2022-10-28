164ff73d0292d10ad516f6673f4e32d32fa4d396
zudata <kxxjx2@gmail.com>
2022-10-28 12:32:17 +0900

    2477

--- Bystep/step14.py
+++ Bystep/step14.py
@@ -26,3 +26,17 @@ while True:
     if l == [0, 0, 0]: break
     elif l[0]**2 + l[1]**2 == l[2]**2: print('right')
     else: print('wrong')
+
+# 2477
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+l = []; w = 0; h = 0; indw = 0; indh = 0
+for i in range(6):
+    l.append(list(map(int, inputs().split())))
+for v, x in enumerate(l):
+    if x[0] > 2 and x[1] > w:
+        w = x[1]; indw = v
+    elif x[0] < 3 and x[1] > h:
+        h = x[1]; indh = v
+print(n*(w*h - l[(indw+3)%6][1]*l[(indh+3)%6][1]))
