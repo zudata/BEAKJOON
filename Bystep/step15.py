62174257e745af061416875ea207960196da46ec
zudata <kxxjx2@gmail.com>
2022-10-28 15:58:53 +0900

    1037

--- Bystep/step15.py
+++ Bystep/step15.py
@@ -7,3 +7,11 @@ while True:
     elif b%a == 0: print('factor')
     elif a%b == 0: print('multiple')
     else: print('neither')
+
+# 1037
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+l = list(map(int, inputs().split()))
+l.sort()
+print(l[0]*l[n-1])
