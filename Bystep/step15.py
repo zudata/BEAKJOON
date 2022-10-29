cfc13d8c8e10c923f5593fd2edf5d6f3daf4601b
zudata <kxxjx2@gmail.com>
2022-10-29 19:58:19 +0900

    2609

--- Bystep/step15.py
+++ Bystep/step15.py
@@ -15,3 +15,16 @@ n = int(inputs())
 l = list(map(int, inputs().split()))
 l.sort()
 print(l[0]*l[n-1])
+
+# 2609
+import sys
+inputs = sys.stdin.readline
+a, b = map(int, inputs().split())
+d = 2; res = 1
+while a >= d and b >= d:
+    if a%d == 0 and b%d == 0:
+        a = a/d; b = b/d
+        res = res*d
+    else: d += 1
+print(res)
+print(int(res*a*b))
