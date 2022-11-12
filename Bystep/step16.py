f17fde2fef60cffcc8648d20fd8cc697d4b8674d
zudata <kxxjx2@gmail.com>
2022-11-12 23:46:40 +0900

    15651

--- Bystep/step16.py
+++ Bystep/step16.py
@@ -30,3 +30,18 @@ def seq(s):
             seq(s)
             x.pop()
 seq(s)
+
+# 15651
+import sys
+inputs = sys.stdin.readline
+n, m = map(int, inputs().split())
+x = []
+def seq():
+    if len(x) == m:
+        print(' '.join(map(str, x)))
+        return
+    for i in range(1, n+1):
+        x.append(i)
+        seq()
+        x.pop()
+seq()
