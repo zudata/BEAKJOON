55b91277aa9cdc004ee1dc11e026dcfc9bcd44f0
zudata <kxxjx2@gmail.com>
2022-11-14 15:13:49 +0900

    9663 time

--- Bystep/step16.py
+++ Bystep/step16.py
@@ -61,3 +61,25 @@ def seq():
             seq()
             x.pop()
 seq()
+
+# 9663
+import sys
+inputs = sys.stdin.readline
+def check(x):
+    for i in range(x):
+        if arr[x] == arr[i] or abs(arr[x]-arr[i]) == x-i:
+            return 0
+    return 1
+def N_Queen(x):
+    global cnt
+    if x == n:
+        cnt += 1
+    else:
+        for i in range(n):
+            arr[x] = i
+            if check(x):
+                N_Queen(x + 1)
+n = int(inputs())
+arr = [0] * n; cnt = 0
+N_Queen(0)
+print(cnt)
