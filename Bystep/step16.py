68c170a67aa7fbbd198782f8e5b4280a6961e6c3
zudata <kxxjx2@gmail.com>
2023-01-05 18:14:52 +0900

    2580

--- Bystep/step16.py
+++ Bystep/step16.py
@@ -87,34 +87,39 @@ print(res)
 
 # 2580
 import sys
-inputs = sys.stdin.readline
-def check(x, y, n):
-    nx = x//3*3
-    ny = y//3*3
-    for i in range(9):
-        if s[x][i] == n: return False
-        elif s[i][y] == n: return False
-    for i in range(3):
-        for j in range(3):
-            if n == s[nx+i][ny+j]: return False
-    return True
-def sudoku(p):
-    if len(z) == 0:
-        for i in range(9):
-            print(*s[i])
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
+zero_pos = [(x,y) for x in range(9) for y in range(9) if board[x][y] == 0]
+z_len = len(zero_pos)
+offset = [(1, 1), (1, -1), (1, 0), (-1, 1), (-1, -1), (-1, 0), (0, 1), (0, -1), (0, 0)]
+offset_pos = [1]*3+[4]*3+[7]*3
+finished = False
+def choice(x,y):
+    nums = list(range(1,10))
+    for n in range(9):
+        if board[x][n] in nums:
+            nums.remove(board[x][n])
+        if board[n][y] in nums:
+            nums.remove(board[n][y])
+    for off in offset:
+        dx = offset_pos[x]+off[0]
+        dy = offset_pos[y]+off[1]
+        if board[dx][dy] in nums:
+            nums.remove(board[dx][dy])
+    return nums
+
+def dfs(cur):
+    global finished
+    if finished == True :
         return
-    for n in range(1, 10):
-        x = z[p][0]; y = z[p][1]
-        if check(x, y, n):
-            z.pop(0)
-            s[x][y] = n
-            sudoku(p+1)
-            s[x][y] = 0
-ss = [list(map(int, inputs().split())) for _ in range(9)]
-s = ss.copy()
-z = []
-for x in range(9):
-    for y in range(9):
-        if s[x][y] == 0:
-            z.append((x, y))
-sudoku(0)
+    if cur == z_len:
+        for item in board:
+            print(*item)
+        finished = True
+        return
+    else:
+        (x,y) = zero_pos[cur]
+        for c in choice(x,y):
+            board[x][y] = c
+            dfs(cur+1)
+            board[x][y] = 0
+dfs(0)
