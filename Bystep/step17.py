a097f813e42cd592bf615770c08acfb790fc7caf
zudata <kxxjx2@gmail.com>
2023-01-18 21:37:34 +0900

    9184 time

--- Bystep/step17.py
+++ Bystep/step17.py
@@ -18,3 +18,23 @@ def fibo2(n):
     return ans2
 fibo1(n)
 print(ans1, fibo2(n))
+
+# 9184
+import sys
+inputs = sys.stdin.readline
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+    elif a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    elif a < b and b < c:
+        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    else:
+        return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+
+while True:
+    a, b, c = map(int, inputs().split())
+    if a == -1 and b == -1 and c == -1: break
+    else:
+        ans = w(a, b, c)
+        print(f'w({a}, {b}, {c}) = {ans}')
