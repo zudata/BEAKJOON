cabbdfc503cc68266e722da8ba6aa0837217caaf
zudata <kxxjx2@gmail.com>
2023-01-18 21:49:06 +0900

    9184

--- Bystep/step17.py
+++ Bystep/step17.py
@@ -22,15 +22,21 @@ print(ans1, fibo2(n))
 # 9184
 import sys
 inputs = sys.stdin.readline
+w_dic = {} # 이미 했던 경로 저장함으로써 시간 단축
 def w(a, b, c):
-    if a <= 0 or b <= 0 or c <= 0:
-        return 1
+    if (a, b, c) in w_dic:
+        return w_dic[(a, b, c)]
+    elif a <= 0 or b <= 0 or c <= 0:
+        w_dic[(a, b, c)] = 1
+        return w_dic[(a, b, c)]
     elif a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
     elif a < b and b < c:
-        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        w_dic[(a, b, c)] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return w_dic[(a, b, c)]
     else:
-        return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        w_dic[(a, b, c)] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        return w_dic[(a, b, c)]
 
 while True:
     a, b, c = map(int, inputs().split())
