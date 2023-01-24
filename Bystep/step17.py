4b08765b2626912b30360e8cd604bcbb6538ec73
zudata <kxxjx2@gmail.com>
2023-01-24 21:34:45 +0900

    1932

--- Bystep/step17.py
+++ Bystep/step17.py
@@ -91,3 +91,17 @@ for i in range(1, n):
     dp[i][1] += min(dp[i-1][0], dp[i-1][2])
     dp[i][2] += min(dp[i-1][0], dp[i-1][1])
 print(min(dp[-1]))
+
+# 1932
+import sys
+inputs = sys.stdin.readline
+dp = []
+for _ in range(int(inputs())):
+    dp.append(list(map(int, sys.stdin.readline().split())))
+for i in range(1, len(dp)):
+    dp[i][0] += dp[i-1][0]
+    dp[i][-1] += dp[i-1][-1]
+    if i >= 2:
+        for j in range(1, i):
+            dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
+print(max(dp[-1]))
