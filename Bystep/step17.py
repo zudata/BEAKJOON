f2035580d021c16402b0fed198f1485c0c802bf7
zudata <kxxjx2@gmail.com>
2023-01-27 21:59:36 +0900

    10844

--- Bystep/step17.py
+++ Bystep/step17.py
@@ -131,3 +131,16 @@ for i in range(2, n+1):
     if i%2 == 0: dp[i] = min(dp[i], dp[i//2]+1)
     if i%3 == 0: dp[i] = min(dp[i], dp[i//3]+1)
 print(dp[n])
+
+# 10844
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+dp = [[0]*10 for _ in range(n)]
+for i in range(1, 10): dp[0][i] = 1
+if n > 1:
+    for i in range(1, n):
+        dp[i][0], dp[i][9] = dp[i-1][1], dp[i-1][8]
+        for j in range(1, 9):
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+print(sum(dp[n-1])%1000000000)
