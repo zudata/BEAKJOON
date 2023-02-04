f9706d4e030de4892d1447fb9417c57282ee2bca
zudata <kxxjx2@gmail.com>
2023-02-04 23:46:39 +0900

    2559

--- BAEKJOON/step18.py
+++ BAEKJOON/step18.py
@@ -10,3 +10,14 @@ for i in l:
 for i in range(m):
     a, b = map(int,inputs().split())
     print(res[b]-res[a-1])
+
+# 2559
+import sys
+inputs = sys.stdin.readline
+n, k = map(int, inputs().split())
+l = list(map(int, inputs().split()))
+s = sum(l[:k]); res = s
+for i in range(n-k):
+    s = s + l[i+k] - l[i]
+    if (res < s): res = s
+print(res)
