2f1491b98c9e0a84b5ab1064442a58969c55eb06
zudata <kxxjx2@gmail.com>
2023-02-05 21:24:20 +0900

    16139

--- BAEKJOON/step18.py
+++ BAEKJOON/step18.py
@@ -21,3 +21,23 @@ for i in range(n-k):
     s = s + l[i+k] - l[i]
     if (res < s): res = s
 print(res)
+
+# 16139
+import sys
+inputs = sys.stdin.readline
+s = inputs().strip()
+q = int(inputs())
+arr = [[0]*26 for i in range(len(s))]
+arr[0][ord(s[0]) - 97] = 1
+for i in range(1, len(s)):
+    arr[i][ord(s[i]) - 97] = 1
+    for j in range(26):
+        arr[i][j] += arr[i - 1][j]
+for i in range(q):
+    a, l, r = inputs().split(); l = int(l); r = int(r)
+    orda = ord(a) - 97
+    if l > 0:
+        res = arr[r][orda] - arr[l - 1][orda]
+    else:
+        res = arr[r][orda]
+    print(res)
