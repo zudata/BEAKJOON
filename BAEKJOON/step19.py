a3deecaaaeff6065ba82991ff092ad4b39817ec3
zudata <kxxjx2@gmail.com>
2023-02-10 10:33:49 +0900

    1931

--- BAEKJOON/step19.py
+++ BAEKJOON/step19.py
@@ -11,3 +11,15 @@ for i in val:
         if k == 0:
             print(res)
             break
+
+# 1931
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+l = [list(map(int, inputs().split())) for _ in range(n)]
+l.sort(key = lambda x : (x[1], x[0])); res = 1; m = l[0][1]
+for i in l[1:]:
+    if i[0] >= m:
+        m = i[1]
+        res += 1
+print(res)
