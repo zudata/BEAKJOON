6a14d9066112b952435f96f649854d0da37a1755
zudata <kxxjx2@gmail.com>
2023-02-14 22:10:44 +0900

    10828

---
+++ BAEKJOON/step20.py
@@ -0,0 +1,27 @@
+# 10828
+import sys
+inputs = sys.stdin.readline
+def push(x):
+    stack.append(x)
+def pop():
+    if len(stack) == 0: return -1
+    else: return stack.pop()
+def size():
+    return len(stack)
+def empty():
+    if stack: return 0
+    else: return 1
+def top():
+    if stack: return stack[-1]
+    else: return -1
+
+n = int(inputs())
+stack = []
+for i in range(n):
+    l = inputs().split()  # 입력값을 띄어쓰기 기준으로 나눔
+    order = l[0]  # 입력값의 명령어
+    if order == "push": push(l[1])
+    elif order == "pop": print(pop())
+    elif order == "size": print(size())
+    elif order == "empty": print(empty())
+    elif order == "top": print(top())
