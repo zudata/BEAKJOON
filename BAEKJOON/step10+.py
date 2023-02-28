ac84b7f9c365a0aa0548faf276ed67620b1f130f
zudata <kxxjx2@gmail.com>
2023-02-28 20:58:23 +0900

    24264-24267

--- BAEKJOON/step10+.py
+++ BAEKJOON/step10+.py
@@ -6,3 +6,27 @@ import sys
 inputs = sys.stdin.readline
 n = int(inputs())
 print(n); print(1)
+
+# 24264
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+print(n**2); print(2)
+
+# 24265
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+print(n*(n-1)//2); print(2)
+
+# 24266
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+print(n**3); print(3)
+
+# 24267
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+print(n*(n-1)*(n-2)//6); print(3)
