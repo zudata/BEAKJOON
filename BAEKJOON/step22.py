b71be7a01387a008262c924911d141a45375e743
zudata <kxxjx2@gmail.com>
2023-03-03 12:54:21 +0900

    10866

--- BAEKJOON/step22.py
+++ BAEKJOON/step22.py
@@ -18,3 +18,38 @@ for _ in range(t):
         else:
             ip.append(ip.pop(0))
             id.append(id.pop(0))
+
+# 10866
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+l = []
+for _ in range(n):
+    m = list(map(str, inputs().split()))
+    if m[0] == 'push_front':
+        l = [m[1]] + l
+    elif m[0] == 'push_back':
+        l += [m[1]]
+    elif m[0] == 'pop_front':
+        try:
+            print(l[0])
+            l = l[1:]
+        except: print(-1)
+    elif m[0] == 'pop_back':
+        try:
+            print(l[-1])
+            l = l[:-1]
+        except: print(-1)
+    elif m[0] == 'size':
+        print(len(l))
+    elif m[0] == 'empty':
+        if len(l) == 0: print(1)
+        else: print(0)
+    elif m[0] == 'front':
+        try:
+            print(l[0])
+        except: print(-1)
+    else:
+        try:
+            print(l[-1])
+        except: print(-1)
