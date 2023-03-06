d5b0db312b62e10bd6d08c2c61b9a989ea3f0333
zudata <kxxjx2@gmail.com>
2023-03-06 13:25:31 +0900

    2630

---
+++ BAEKJOON/step23.py
@@ -0,0 +1,23 @@
+# 2630
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+arr = [list(map(int, inputs().split())) for _ in range(n)]
+c1 = 0; c2 = 0
+def square(x, y, n):
+    global c1, c2
+    col = arr[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if col != arr[i][j]:
+                n //= 2
+                square(x, y, n)
+                square(x+n, y, n)
+                square(x, y+n, n)
+                square(x+n, y+n, n)
+                return
+    if col == 0: c1 += 1
+    else: c2 += 1
+    return
+square(0, 0, n)
+print(c1, c2, sep = '\n')
