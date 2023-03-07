5afa295da2221a8de2350c475dee16c27983a501
zudata <kxxjx2@gmail.com>
2023-03-07 09:04:09 +0900

    1992

--- BAEKJOON/step23.py
+++ BAEKJOON/step23.py
@@ -21,3 +21,21 @@ def square(x, y, n):
     return
 square(0, 0, n)
 print(c1, c2, sep = '\n')
+
+# 1992
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+arr = [inputs() for _ in range(n)]
+def paper(x, y, n):
+    n //= 2
+    if n == 0:
+        return arr[x][y]
+    q1 = paper(x, y, n)
+    q2 = paper(x, y+n, n)
+    q3 = paper(x+n, y, n)
+    q4 = paper(x+n, y+n, n)
+    if (q1 in ['0', '1']) and q1 == q2 == q3 == q4:
+        return q1
+    return '(' + q1 + q2 + q3 + q4 + ')'
+print(paper(0, 0, n))
