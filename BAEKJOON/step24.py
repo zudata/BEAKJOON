12521601e44f747b987520184c92a4ef45fd6f5b
zudata <kxxjx2@gmail.com>
2023-04-04 22:13:31 +0900

    1654

--- BAEKJOON/step24.py
+++ BAEKJOON/step24.py
@@ -8,3 +8,17 @@ ml = list(map(int, inputs().split()))
 for i in ml:
     if i in nl: print(1)
     else: print(0)
+
+# 1654
+import sys
+inputs = sys.stdin.readline
+k, n = map(int, inputs().split())
+l = [int(inputs()) for _ in range(k)]
+t = 1; res = max(l)
+while t <= res:
+    mid = (t + res) // 2
+    ln = 0
+    for i in l: ln += i // mid
+    if ln >= n: t = mid + 1
+    else: res = mid - 1
+print(res)
