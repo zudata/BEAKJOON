89f605bc7705a321c1c224dddc354a8878d4cc68
zudata <kxxjx2@gmail.com>
2023-04-07 17:53:40 +0900

    1300

--- BAEKJOON/step24.py
+++ BAEKJOON/step24.py
@@ -54,3 +54,16 @@ while start <= end:
         start = mid + 1; res = mid
     else: end = mid - 1
 print(res)
+
+# 1300
+import sys
+inputs = sys.stdin.readline
+n = int(inputs()); k = int(inputs())
+start = 1; end = k
+while start <= end:
+    mid = (start + end) // 2; cnt = 0
+    for i in range(1,n+1): cnt += min(n, mid//i)
+    if cnt >= k:
+        res = mid; end = mid-1
+    else: start = mid+1
+print(res)
