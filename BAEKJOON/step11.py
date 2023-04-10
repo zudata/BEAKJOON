ec01bfbcf6278cfd9ac8816d9cc1737f640152df
zudata <kxxjx2@gmail.com>
2023-04-10 21:40:44 +0900

    4779

--- BAEKJOON/step11.py
+++ BAEKJOON/step11.py
@@ -113,3 +113,20 @@ def fact(n):
         res = n*fact(n-1)
     return res
 print(fact(int(input())))
+
+# 4779
+import sys
+inputs = sys.stdin.readline
+def sol(n, i, j):
+    if n == 0: return
+    cnt = (j - i + 1) // 3
+    sol(n - 1, i, i + cnt - 1)
+    for k in range(i + cnt, i + cnt * 2): res[k] = ' '
+    sol(n - 1, i + cnt * 2, i + cnt * 3 - 1)
+while True:
+    try:
+        n = int(inputs())
+        res = ['-'] * (3 ** n)
+        sol(n, 0, 3 ** n - 1)
+        print(''.join(res))
+    except: break
