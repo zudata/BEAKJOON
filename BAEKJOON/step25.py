ee77622dcb67f5af3c5fde1fa7f94d2cd1e28905
zudata <kxxjx2@gmail.com>
2023-04-16 21:31:08 +0900

    1927

--- BAEKJOON/step25.py
+++ BAEKJOON/step25.py
@@ -14,3 +14,15 @@ for i in range(n):
         heapq.heappop(heap)
     elif x == 0 and len(heap) == 0:
         print(0)
+
+# 1927
+import sys, heapq
+inputs = sys.stdin.readline
+n = int(inputs())
+heap = []
+for _ in range(n):
+    cmd = int(inputs())
+    if not cmd:
+        if heap: print(heapq.heappop(heap))
+        else: print(0)
+    else: heapq.heappush(heap, cmd)
