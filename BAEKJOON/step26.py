a7278871ba7ffee0c41afee1242a626ecc40719a
zudata <kxxjx2@gmail.com>
2023-04-20 17:23:36 +0900

    1520

--- BAEKJOON/step26.py
+++ BAEKJOON/step26.py
@@ -29,3 +29,23 @@ for i in range(1, n+1):
             for k in range(j, j+i):
                 dp[j][j+i] = min(dp[j][j+i], dp[j][k] + dp[k+1][j+i] + arr[j][0] * arr[k][1] * arr[j+i][1]) 
 print(dp[0][n-1])
+
+# 1520
+import sys
+inputs = sys.stdin.readline
+m, n = map(int, inputs().split())
+arr = [list(map(int, inputs().split())) for _ in range(m)]
+route=[[-1]*n for _ in range(m)]
+dx=[1,-1,0,0]; dy=[0,0,1,-1]
+def dfs(start):
+    x, y = start
+    if x == m-1 and y == n-1: return 1
+    if route[x][y] != -1: return route[x][y]
+    route[x][y] = 0
+    for i in range(4):
+        xx = x + dx[i]; yy = y + dy[i]
+        if xx < 0 or xx >= m or yy < 0 or yy >= n: continue
+        if arr[xx][yy] < arr[x][y]: route[x][y] += dfs([xx,yy])
+    return route[x][y]
+dfs([0,0])
+print(route[0][0])
