64d9cf8a8f645233d2ba872233facdf98aba0ba3
zudata <kxxjx2@gmail.com>
2023-04-25 18:25:18 +0900

    17298

--- BAEKJOON/step29.py
+++ BAEKJOON/step29.py
@@ -10,3 +10,15 @@ for i in range(len(s)):
         for i in range(len(bomb)): res.pop()
 if res: print(''.join(res))
 else: print('FRULA')
+
+# 17298
+import sys
+inputs = sys.stdin.readline
+n = int(inputs())
+a = list(map(int, inputs().split()))
+res = [-1]*n; stack = []
+for i in range(n):
+    while stack and a[stack[-1]] < a[i]:
+        res[stack.pop()] = a[i]
+    stack.append(i)
+print(*res)
