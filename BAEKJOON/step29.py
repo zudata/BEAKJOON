b7f5b39f0b73427ec83dab07836848f35d75ad69
zudata <kxxjx2@gmail.com>
2023-04-26 23:12:31 +0900

    17299

--- BAEKJOON/step29.py
+++ BAEKJOON/step29.py
@@ -22,3 +22,17 @@ for i in range(n):
         res[stack.pop()] = a[i]
     stack.append(i)
 print(*res)
+
+# 17299
+import sys
+import collections
+inputs = sys.stdin.readline
+n = int(inputs())
+a = list(map(int, inputs().split()))
+cnt = collections.Counter(a)
+res = [-1]*n; stack = [0]
+for i in range(1, n):
+    while stack and cnt[a[stack[-1]]] < cnt[a[i]]:
+        res[stack.pop()] = a[i]
+    stack.append(i)
+print(*res)
