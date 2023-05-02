be9dc37cec2090efa41fd6e58307408103fe6d09
zudata <kxxjx2@gmail.com>
2023-05-02 18:06:01 +0900

    24445

--- BAEKJOON/step30.py
+++ BAEKJOON/step30.py
@@ -67,3 +67,29 @@ def algorithm(v):
                 res[i] = cnt 
 algorithm(r)
 print(*res[1:], sep = '\n')
+
+# 24445
+import sys
+from collections import deque
+inputs = sys.stdin.readline
+n, m, r = map(int, inputs().split())
+graph = [[] for _ in range(n+1)]
+res = [0]*(n+1)
+for _ in range(m):
+    u, v = map(int, inputs().split())
+    graph[u].append(v); graph[v].append(u)
+for i in graph: 
+    i.sort(reverse = True)
+def algorithm(v):
+    cnt = 1
+    queue = deque([v])
+    res[v] = cnt 
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if res[i] == 0:
+                cnt += 1
+                queue.append(i)
+                res[i] = cnt 
+algorithm(r)
+print(*res[1:], sep = '\n')
